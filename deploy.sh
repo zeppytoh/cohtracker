c4b00d79e90261a26ddbfcfbbb5599eff5507cae
Victor Toh <victortoh@gmail.com>
2019-01-18 11:51:02 +0800

    Setup for continuous integration

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,6 @@ git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
-git push -f git@github.com:zeppytoh/cohtracker.git master:gh-pages
+git push -f https://github.com/zeppytoh/cohtracker.git master:gh-pages
 
 cd -
